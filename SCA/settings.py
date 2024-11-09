d802cf90de6fde83305fbdadfea1c4c7df0c446f
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2024-11-09 15:56:34 +0200

    Secret key

--- SCA/settings.py
+++ SCA/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = "django-insecure-ijsk#b94)7@^rd9anwk45*tm=k+tb14=m^5mt4((d@sf9f^z5q"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
